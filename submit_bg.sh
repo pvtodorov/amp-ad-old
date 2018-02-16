7656f0e368b93ce0660fd8f282c596ed125081b8
Unknown <petar.v.todorov@gmail.com>
2018-02-16 16:42:21 -0500

    remove file for submitting jobs to O2, update with new file

--- submit_bg.sh
+++ submit_bg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -c 1
+#SBATCH -c 4
 #SBATCH -N 1
-#SBATCH -t 8:00:00
+#SBATCH -t 12:00:00
 #SBATCH -p short
 #SBATCH --mem=4000
 #SBATCH -o logs/hostname_%j.out
 #SBATCH -e logs/hostname_%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=petar_todorov@hms.harvard.edu
+#SBATCH --mail-user=first.last@example.com
 #SBATCH --array=1-100
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=4
 uptime
-python predict.py run_settings/settings_LPOCV_BM36_Ordinal.json
+btr-predict run_settings/settings_ROSMAP_LPOCV_BM9_46_Ordinal.json
 uptime
